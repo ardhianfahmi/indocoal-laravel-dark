1d584d2162f4800fbe9859bca198301cf5934816
ardhianfahmi <ardhianfahmi.sabani072@gmail.com>
2022-03-28 18:12:16 +0700

    helo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -490,7 +490,10 @@
         <div class="row" data-aos="fade-up">
 
           <div class="col-lg-6 ">
-            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31688.161028063107!2d111.5292083569209!3d-6.888192125532648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7714f355674699%3A0x5027a76e355e180!2sTahunan%2C%20Sale%2C%20Kabupaten%20Rembang%2C%20Jawa%20Tengah!5e0!3m2!1sid!2sid!4v1605857197385!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+            <link rel="stylesheet" type="text/css" href="{{ ('/css/map.css') }}" />
+            <script src="{{ ('/js/map.js') }}"></script>
+            <iframe id="map" class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31688.161028063107!2d111.5292083569209!3d-6.888192125532648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7714f355674699%3A0x5027a76e355e180!2sTahunan%2C%20Sale%2C%20Kabupaten%20Rembang%2C%20Jawa%20Tengah!5e0!3m2!1sid!2sid!4v1605857197385!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
             <!-- <iframe  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe> -->
           </div>
 
