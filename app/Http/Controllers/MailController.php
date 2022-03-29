d6cca4644fd4109522c2456c553a708908a40b8e
ardhianfahmi <ardhianfahmi.sabani072@gmail.com>
2022-03-30 04:32:35 +0700

    commit

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -14,7 +14,7 @@ class MailController extends Controller
     {
 
         $details = [
-
+            'name' => $_POST['name'],
             'title' => $_POST['subject'],
             'body' => $_POST['message']
         ];
@@ -22,9 +22,9 @@ class MailController extends Controller
         Mail::to($_POST['email'])->send(new \App\Mail\MyTestMail($details));
 
         $details2 = [
-
+            'name' => $_POST['name'],
             'title' => $_POST['subject'],
-            'body' => '<html>new email from ' . $_POST['email'] . '<br>' . ' Title: ' . $_POST['subject'] . '<br>' . ' Body: ' . $_POST['message'] . '</html>'
+            'body' => '<html> <br>Email Pengirim : ' . $_POST['email'] . '<br>' . ' Isi Pesan: ' . $_POST['message'] . '</html>'
         ];
 
         Mail::to(" indocoal8@gmail.com")->send(new \App\Mail\MyTestMail($details2));
--- resources/views/emails/myTestMail.blade.php
+++ resources/views/emails/myTestMail.blade.php
@@ -6,7 +6,8 @@
 </head>
 
 <body>
-    <h1>{{ $details['title'] }}</h1>
+    <h1>{{ $details['name'] }}</h1>
+    <h3>Subject : {{ $details['title'] }}</h3>
     <p><?php echo $details['body']; ?></p>
 
     <p>Thank you</p>
